e742956ba9ba6f7036ace87e766d3c4eec0a85cd
BradyLange1 <bradylange99@gmail.com>
2023-05-12 10:27:12 -0500

    added user length prompt and added error handling to users input

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Generates Password
+function generatePassword(){
+  var userLength = window.prompt("Enter how long you want your password to be (8-128): ")
+  console.log(userLength)
+  if(userLength === null){
+    return;
+  } else if(typeof userLength !== "number"){
+    window.alert("Must enter a NUMBER between 8 and 128 characters")
+    return;
+  } else if(userLength < 8 || userLength> 128){
+    window.alert("Must enter a number between 8 and 128 characters")
+    return;
+  } 
+  window.prompt("Example")
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
