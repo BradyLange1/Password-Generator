d0e7c5d5167209aaf1b2789e41745a4b2b391621
BradyLange1 <bradylange99@gmail.com>
2023-05-12 10:56:28 -0500

    added user selected parameters for which character types to be in password

--- script.js
+++ script.js
@@ -3,18 +3,43 @@ var generateBtn = document.querySelector("#generate");
 
 //Generates Password
 function generatePassword(){
+
+  var userParameters = []
   var userLength = window.prompt("Enter how long you want your password to be (8-128): ")
   console.log(userLength)
-  if(userLength === null){
+  console.log(typeof userLength)
+  if (userLength === null){
     return;
-  } else if(typeof userLength !== "number"){
+  }
+  //need to fix error handling for if user enters something other than numbers
+  else if (typeof userLength !== "string"){
     window.alert("Must enter a NUMBER between 8 and 128 characters")
     return;
-  } else if(userLength < 8 || userLength> 128){
+  } else if (userLength < 8 || userLength > 128){
     window.alert("Must enter a number between 8 and 128 characters")
     return;
   } 
-  window.prompt("Example")
+  userParameters.push(userLength)
+  console.log(userParameters)
+
+  var includeNames = ["lowercase", "uppercase", "numeric", "special"]
+  
+  for (var i = 0; i < 4; i++){
+    var userInclude = window.prompt("Click OK if you would like to include " + includeNames[i] + " characters")
+    if (userInclude === null){
+      userParameters.push(false)
+    } else {
+      userParameters.push(true)
+    }
+  }
+
+  // if (userParameters.includes(true) === false){
+  //   window.alert("Must allow one character type")
+  //   return;
+  // }
+  console.log(userParameters)
+
+
 }
 
 // Write password to the #password input
